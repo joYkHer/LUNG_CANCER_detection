30a2e02aec59b77a05377ef359ab9fd31eb000b3
joYkHer <36397736+joYkHer@users.noreply.github.com>
2019-08-19 20:25:03 +0800

    Add files via upload

--- nets.py
+++ nets.py
@@ -1,5 +1,6 @@
 import numpy as np
-from  torch.utils.data import Dataset
+import torch
+from torch.utils.data import Dataset
 from torchvision import transforms
 import os
 
@@ -8,10 +9,11 @@ pid_to_label = np.load(r'./pid_to_label_one.npy', allow_pickle=True).tolist()
 # 到时候还是按照label的顺序来读取好了，免得各种原因顺序乱了
 
 class custom_dataset(Dataset):
-    def __init__(self, all_p_slices, pid_t0_labels):
+    def __init__(self, all_p_slices, pid_to_labels, transform): # transform, 一个实例
         super(custom_dataset, self).__init__()
         self.all_patient_slices = all_p_slices
-        self.pid_to_lable = pid_t0_labels
+        self.pid_to_lable = pid_to_labels
+        self.transform = transform
 
     def __len__(self): # len(class_instance)
         return len(self.all_patient_slices) # part:205个
@@ -24,8 +26,28 @@ class custom_dataset(Dataset):
         '''
         assert os.listdir(r'./stage1') == list(pid_to_label.keys()), 'ooops'
         # 保证与最开始的目录顺序一致， 不要手贱去改..
-        return { 'slices':self.all_patient_slices[idx], 'label': list(pid_to_label.values())[idx]}
+        # self.transform 得定义__call__()...
+        return self.transform({'slices':self.all_patient_slices[idx], 'label': list(pid_to_label.values())[idx]}) # transform实例__call__()
 
 
-p_dataset = custom_dataset(all_normalized_pslices, pid_to_label)
+class to_tensor(object):
+    def __init__(self):
+        super(to_tensor, self).__init__()
+
+    def __call__(self, pslices_and_label): # 定义__call__,  实例()就是嗲用该函数
+        # type()为字典, pslices: array, label:str
+        pslices, label = pslices_and_label['slices'], pslices_and_label['label']
+        #pslices = pslices.transpose((2,0,1))
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        # TODO need to check out, codes below too
+        return {'slices':torch.from_numpy(pslices), 'label': torch.tensor(int(label))}
+
+
+p_dataset = custom_dataset(
+    all_normalized_pslices,
+    pid_to_label,
+    transform=transforms.Compose([
+        to_tensor()
+    ]))
 print(p_dataset[0])
